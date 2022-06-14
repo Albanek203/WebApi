95b8465ea15448572a253174f6517cedc950d832
Oleh <73631796+Albanek203@users.noreply.github.com>
2022-06-14 21:43:55 +0300

    Added configuration for dependency injection

---
+++ WebApi/Infrastructure/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using WebApi.Repository;
+using WebApi.Services;
+
+namespace WebApi.Infrastructure; 
+
+public static class Configuration {
+    public static void ConfigurationService(IServiceCollection services) {
+        // Repository
+        services.AddTransient<IncidentRepository>();
+        services.AddTransient<AccountRepository>();
+        services.AddTransient<ContactRepository>();
+        
+        // Services
+        services.AddTransient<IncidentService>();;
+        services.AddTransient<AccountService>();;
+        services.AddTransient<ContactService>();;
+    }
+}
