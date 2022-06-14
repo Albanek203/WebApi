7091d8b828289aff3a3999c34e880d8fa905ab82
Oleh <73631796+Albanek203@users.noreply.github.com>
2022-06-14 21:42:56 +0300

    Added context and overrides OnModelCreating method

---
+++ WebApi/Context/IncidentContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Context;
+
+public class IncidentContext : DbContext {
+    public IncidentContext(DbContextOptions<IncidentContext> options) : base(options) { Database.EnsureCreated(); }
+
+    public DbSet<Incident> Incidents { get; set; }
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        modelBuilder.Entity<Incident>().HasKey(x=>x.Name);
+        modelBuilder.Entity<Incident>().Property(x=>x.Name).HasDefaultValueSql("NEWID()");
+
+        modelBuilder.Entity<Account>().HasIndex(x=>x.Name).IsUnique();
+        modelBuilder.Entity<Contact>().HasIndex(x=>x.Email).IsUnique();
+    }
+}
