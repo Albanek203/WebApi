eaade01e379706f886b36332d6de3dff5054f428
Oleh <73631796+Albanek203@users.noreply.github.com>
2022-06-14 21:46:21 +0300

    Added incident controller Implemented Get / GetAll / Post functions

---
+++ WebApi/Controllers/IncidentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Models.ViewModels;
+using WebApi.Services;
+using ClassMapper;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class IncidentController : ControllerBase {
+    private readonly IncidentService _incidentService;
+    private readonly AccountService _accountService;
+
+    public IncidentController(IncidentService incidentService
+                            , AccountService accountService) {
+        _incidentService = incidentService;
+        _accountService = accountService;
+    }
+
+    [HttpGet("{name}")]
+    public async Task<IActionResult> Get(string name) {
+        var incident = (await _incidentService.FindByConditionAsync(x => x.Name.ToString() == name)).FirstOrDefault();
+        if (incident == null) return NotFound();
+        return Ok(incident);
+    }
+
+    [HttpGet]
+    public async Task<IReadOnlyCollection<IncidentViewModel>> GetAll() {
+        var incidents = await _incidentService.GetAllAsync();
+        return incidents.Select(incident => incident.Map<Incident, IncidentViewModel>()).ToList();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] IncidentViewModel viewModel) {
+        var account = (await _accountService.FindByConditionAsync(x => x.Name == viewModel.Name)).FirstOrDefault();
+        if (account == null) { return NotFound(); }
+        var incident = new Incident {
+            Accounts = new List<Account> { account }, Description = viewModel.Description
+        };
+        await _incidentService.CreateAsync(incident);
+        return CreatedAtAction(nameof(Get), new { name = incident.Name }, incident);
+    }
+}
